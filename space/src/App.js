6f9ad401549d033c90012794452620f4f5672d93
Fuzailkazi <mohdfuzailkazi14@gmail.com>
2022-08-19 11:52:07 +0530

    files changed

--- space/src/App.js
+++ space/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [news, setNews] = useState([]);
+  useEffect(() => {
+    fetch("https://api.spaceflightnewsapi.net/v3/articles")
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        setNews(data);
+      }, []);
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='title'>
+        <h1>Space News</h1>
+      </div>
+      <div className='newsContainer'>
+        {news.map((val, key) => {
+          return (
+            <div
+              key={key}
+              className='article'
+              onClick={() => {
+                window.location.href = val.url;
+              }}
+            >
+              <h3>{val.title}</h3>
+              <img src={val.imageUrl} />
+              <p>{val.summary}</p>
+              <h5>{val.publishedAt}</h5>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
